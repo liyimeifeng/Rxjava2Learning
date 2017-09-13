166dce16cf98dafb62cfe11e25b7bf283ee18ce3
liyi <liyi@dftcmedia.com>
2017-09-13 14:55:20 +0800

    增加两个module学习

--- app/src/main/java/com/li/rxjava2/MainActivity.java
+++ app/src/main/java/com/li/rxjava2/MainActivity.java
@@ -21,6 +21,7 @@ import javax.security.auth.login.LoginException;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import io.reactivex.Maybe;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
@@ -122,49 +123,6 @@ public class MainActivity extends AppCompatActivity {
 
                     }
                 });
-
-
-
-
-//        ApiManager apiManager = RetrofitWrapper.getInstance().getRetrofit().create(ApiManager.class);
-//        apiManager.getWeatherByName(Constant.city)
-//                .subscribeOn(Schedulers.io())
-//                .doOnSubscribe(new Action0() {  //默认情况下， doOnSubscribe() 执行在 subscribe() 发生的线程；而如果在 doOnSubscribe() 之后有 subscribeOn() 的话，它将执行在离它最近的 subscribeOn() 所指定的线程。
-//                    @Override
-//                    public void call() {
-//                        Log.e(TAG, "call: ----->" + Thread.currentThread() );
-//                        Toast.makeText(MainActivity.this, "正在获取信息....", Toast.LENGTH_SHORT).show();
-//                    }
-//                })
-//                .map(new Func1<WeatherData, WeatherData>() {
-//                    @Override
-//                    public WeatherData call(WeatherData weatherData) {
-//                        return null;
-//                    }
-//                })
-//
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Subscriber<WeatherData>() {
-//                    @Override
-//                    public void onCompleted() {
-//
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//                        Log.e(TAG, "onError: e============" + e);
-//                    }
-//
-//                    @Override
-//                    public void onNext(WeatherData weatherData) {
-//                        Log.e(TAG, "onNext: ==========" + weatherData.toString());
-//                        Text_Temperature.setText( "温度（华氏度）：" + weatherData.getMain().getTemp());
-//                        Text_WeatherDescription.setText("天气：" + weatherData.getWeather().get(0).getDescription());
-//                        Text_Humidity.setText( "湿度：" + weatherData.getMain().getHumidity());
-//                        Text_Pressure.setText( "气压：" + weatherData.getMain().getPressure());
-//                        Text_Sunset.setText("日落时间：" + dataFormate(weatherData.getSys().getSunset()));
-//                    }
-//                });
 
     }
 
