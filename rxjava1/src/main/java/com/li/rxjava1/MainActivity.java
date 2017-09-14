96d04226ed5784b7ad37599255ebd01b3b19fcff
liyi <liyi@dftcmedia.com>
2017-09-14 17:59:57 +0800

    add backpressure

--- rxjava1/src/main/java/com/li/rxjava1/MainActivity.java
+++ rxjava1/src/main/java/com/li/rxjava1/MainActivity.java
@@ -2,17 +2,58 @@ package com.li.rxjava1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String TAG = MainActivity.class.getSimpleName();
+    private Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button)findViewById(R.id.activity_main_button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                backPressure();
+            }
+        });
     }
 
+    private void backPressure(){
+        Observable.interval(1, TimeUnit.MILLISECONDS)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Subscriber<Long>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(Long aLong) {
+
+                    }
+                });
+
+    }
 
     private void test(){
         Observer observer =new Observer() {
